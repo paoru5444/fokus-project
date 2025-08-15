595bdeabd8bab1b9d185afa4adacb3125e29837b
Paulo Roberto <paulo.pereira.dev@gmail.com>
2025-08-14 21:41:03 -0300

    feature: exercise 01, catching elements with javascript

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const html = document.querySelector("html");
+const timer = document.querySelector(".app__card-timer");
+
+// header
+const headerImage = document.querySelector(".app__image");
+const headerTitleRegular = document.querySelector(".app__title");
+const headerTitleStrong = document.querySelector(".app__title-strong");
+
+// buttons
 const buttonFocus = document.querySelector(".app__card-button--foco");
 const buttonShortBreak = document.querySelector(".app__card-button--curto");
 const buttonLongBreak = document.querySelector(".app__card-button--longo");

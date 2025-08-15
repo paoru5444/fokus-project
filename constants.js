152b80ca3583c9997de28eed7a1099da19eace1d
Paulo Roberto <paulo.pereira.dev@gmail.com>
2025-08-15 09:00:33 -0300

    feature: add dinamic header title changes according to context

---
+++ constants.js
@@ -0,0 +1,14 @@
+export const FOCUS_TITLE = {
+  regular: "Otimize sua produtividade,",
+  strong: "mergulhe no que importa.",
+};
+
+export const SHORT_BREAK_TITLE = {
+  regular: "Que tal dar uma respirada?",
+  strong: "Faça uma pausa curta!",
+};
+
+export const LONG_BREAK_TITLE = {
+  regular: "Hora de voltar à superfície.",
+  strong: "Faça uma pausa longa.",
+};
--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+import {
+  FOCUS_TITLE,
+  SHORT_BREAK_TITLE,
+  LONG_BREAK_TITLE,
+} from "./constants.js";
+
 const html = document.querySelector("html");
 
 // header
 const headerImage = document.querySelector(".app__image");
-const headerTitleRegular = document.querySelector(".app__title");
+const headerTitleRegular = document.querySelector(".app__title-regular");
 const headerTitleStrong = document.querySelector(".app__title-strong");
 
 // buttons
@@ -18,14 +24,35 @@ const focusTime = 1500;
 const shortBreakTime = 300;
 const shortLongTime = 900;
 
-buttonFocus.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "foco");
-});
+function changeHeaderTitle(context) {
+  switch (context) {
+    case "foco":
+      headerTitleRegular.innerHTML = FOCUS_TITLE.regular;
+      headerTitleStrong.innerHTML = FOCUS_TITLE.strong;
+      break;
+    case "descanso-curto":
+      headerTitleRegular.innerHTML = SHORT_BREAK_TITLE.regular;
+      headerTitleStrong.innerHTML = SHORT_BREAK_TITLE.strong;
+      break;
+    case "descanso-longo":
+      headerTitleRegular.innerHTML = LONG_BREAK_TITLE.regular;
+      headerTitleStrong.innerHTML = LONG_BREAK_TITLE.strong;
+      break;
+  }
+}
 
-buttonShortBreak.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-curto");
-});
+function changeContext(context) {
+  html.setAttribute("data-contexto", context);
+  headerImage.setAttribute("src", `/imagens/${context}.png`);
+  changeHeaderTitle(context);
+}
 
-buttonLongBreak.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-longo");
-});
+buttonFocus.addEventListener("click", () => changeContext("foco"));
+
+buttonShortBreak.addEventListener("click", () =>
+  changeContext("descanso-curto")
+);
+
+buttonLongBreak.addEventListener("click", () =>
+  changeContext("descanso-longo")
+);
